a47c20ea08405023bf59ecac2c0acaa9c16ca212
luxi-huang <lucy0307@qq.com>
2020-12-01 21:50:59 -0600

    update file

--- build/whisker/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/luxi/github_projects/Whisker_Robot/src/whisker
-# Build directory: /home/luxi/github_projects/Whisker_Robot/build/whisker
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/whisker/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/luxi/github_projects/Whisker_Robot/src/whisker/include".split(';') if "/home/luxi/github_projects/Whisker_Robot/src/whisker/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "whisker"
-PROJECT_SPACE_DIR = "/home/luxi/github_projects/Whisker_Robot/devel"
-PROJECT_VERSION = "0.0.0"
